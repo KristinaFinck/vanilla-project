02183ebcd85ab32bb416442a8030df3be863d3f0
KristinaFinck <kristina.finck83@gmail.com>
2024-05-04 18:06:32 +0300

    map part 1 lesson 5

---
+++ src/lesson_5_map/05.test.ts
@@ -0,0 +1,20 @@
+import {ManType} from "./05";
+
+let people: Array<ManType> = [];
+
+beforeEach(() => {
+    people = [
+        {name: 'Andrew Ivanov', age: 33},
+        {name: 'Alexander Petrov', age: 24},
+        {name: 'Dmitry Sidorov', age: 18},
+    ]
+})
+
+test('should get array of greeting messages', () => {
+    const message = people.map(man => `Hello ${man.name.split(" ")[0]}. Welcome to IT`);
+    expect(message.length).toBe(3);
+
+    expect(message[0]).toBe('Hello Andrew. Welcome to IT');
+    expect(message[1]).toBe('Hello Alexander. Welcome to IT');
+    expect(message[2]).toBe('Hello Dmitry. Welcome to IT');
+})
